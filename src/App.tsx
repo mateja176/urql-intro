be763c5eb1b36483baca004455f82a6f0d9ad31d
mateja <mateja176@gmail.com>
2019-09-15 17:37:35 +0200

    feat(App): enables fetching repos by username

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,70 @@ import React from 'react';
 import { useQuery } from 'urql';
 
 const query = gql`
-  {
-    viewer {
-      name
+  query($login: String!) {
+    user(login: $login) {
+      repositories(first: 50, isFork: false) {
+        nodes {
+          name
+          url
+        }
+      }
     }
   }
 `;
 
+const initialValue = 'mateja176';
+
 const App: React.FC = () => {
-  const [{ data, fetching, error }] = useQuery<{ viewer: { name: string } }>({
+  const [value, setValue] = React.useState(initialValue);
+
+  const [login, setLogin] = React.useState(initialValue);
+
+  const [{ data, fetching, error }] = useQuery<
+    { user: { repositories: { nodes: Array<{ name: string; url: string }> } } },
+    { login: string }
+  >({
     query,
+    variables: {
+      login,
+    },
   });
 
   return (
-    <div>
-      {fetching ? (
-        'Loading...'
-      ) : error ? (
-        <pre>JSON.stringify(error, null, 2)</pre>
-      ) : (
-        data!.viewer.name
-      )}
-    </div>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+
+        setLogin(value);
+      }}
+    >
+      <input
+        value={value}
+        onChange={({ target: { value } }) => {
+          setValue(value);
+        }}
+      />
+      <input type="submit" />
+      <div>
+        {fetching ? (
+          'Loading...'
+        ) : error ? (
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        ) : (
+          <ul>
+            {data!.user.repositories.nodes.map(node => (
+              <li key={node.url}>
+                <ul>
+                  {Object.entries(node).map(entry => (
+                    <li key={entry[0]}>{entry.join(': ')}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </form>
   );
 };
 
